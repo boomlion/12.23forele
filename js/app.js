2a3e427c652d791890458cad15a487f8b4751e75
lion <280077219@qq.com>
2018-12-23 22:01:36 +0800

    自己功能的完善

--- js/app.js
+++ js/app.js
@@ -1,6 +1,61 @@
-(function (window) {
-	'use strict';
+; (function (window) {
+	// var list = JSON.parse(localStorage.getItem('item')) || [];
+	var vm = new Vue({
+		el: '#app',
+		data: {
+			name: '',
+			editId: -1,
+			list: []
+		},
+		created() {
+			this.list = JSON.parse(localStorage.getItem('item')) || [];
+			axios
+				.get(' http://localhost:3000/list')
+				.then(function (res) {
+				this.list=res.data;
+				})
+		},
+		methods: {
+			add() {
+				if (this.name.length === 0) {
+					this.name = ''
+					return
+				}
+				this.list.push({ id: this.list[this.list.length - 1] + 1, name: this.name, done: false })
+				this.name = ''
 
-	// Your starting point. Enjoy the ride!
+			},
+			del(id) {
+				this.list.splice(this.list.findIndex(item => item.id == id), 1)
+			},
+			clear() {
+				this.list = this.list.filter(v => v.done === false)
+			}
+		},
+		computed: {
+			num() {
+				return this.list.filter(item => item.done == false).length
+			},
+			complated() {
+				return this.list.some(v => v.done == true)
+			},
+			have() {
+				return this.list.length
+			}
 
+		},
+		watch: {
+			list: {
+				deep: true,
+				handler(vle) {
+					localStorage.setItem('item', JSON.stringify(vle));
+				}
+			}
+		}
+
+	})
+
+
+
+	window.vm = vm
 })(window);
